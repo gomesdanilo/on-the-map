ddf61fd6e69a0c8b6afa09599efbbc105033560e
Danilo Gomes <danilo_rs_gomes@hotmail.com>
2017-09-06 12:10:21 +0100

    new button styles

---
+++ on-the-map/StudentInformation.swift
@@ -0,0 +1,22 @@
+//
+//  StudentInformation.swift
+//  on-the-map
+//
+//  Created by Danilo Gomes on 05/09/2017.
+//  Copyright © 2017 Danilo Gomes. All rights reserved.
+//
+
+import UIKit
+
+struct StudentInformation {
+
+    var name : String?
+    var mediaUrl : String?
+    var lat : Double?
+    var long : Double?
+    
+    init(dictionary : [String : Any]) {
+        
+    }
+}
+
