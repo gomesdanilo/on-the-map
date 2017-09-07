1985e219bccb7740e1c943b7e5ce64a19b58c2bb
Danilo Gomes <danilo_rs_gomes@hotmail.com>
2017-09-07 12:33:27 +0100

    Fix geocode errors

--- on-the-map/AddPinGeocodeController.swift
+++ on-the-map/AddPinGeocodeController.swift
@@ -15,13 +15,21 @@ class AddPinGeocodeController: NSObject {
                      completionHandler : @escaping (_ coordinates : CLLocationCoordinate2D?, _ errorMessage : String?) -> Void){
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(address, completionHandler: { (placemarks, error) in
-            if error != nil || placemarks?.count == 0 {
-                
+            
+            if error != nil {
                 DispatchQueue.main.async {
-                    completionHandler(nil, "Failed to find address")
+                    completionHandler(nil, "Failed to find address: \(error!.localizedDescription)")
                 }
                 return
             }
+            
+            if placemarks != nil && placemarks!.count == 0{
+                DispatchQueue.main.async {
+                    completionHandler(nil, "Address not found")
+                }
+                return
+            }
+            
             let place = MKPlacemark(placemark: placemarks![0])
             DispatchQueue.main.async {
                 completionHandler(place.coordinate, nil)
